a9e4e261f5b19c1c7747e75091679ffeb2e23b58
mwachits <davymwachits@gmail.com>
2024-09-14 02:09:00 +0300

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,8 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
-
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
 
 function App() {
   const [tasks, setTasks] = useState(TASKS);
@@ -26,18 +23,18 @@ function App() {
   const filteredTasks = selectedCategory === 'All'
     ? tasks
     : tasks.filter(task => task.category === selectedCategory);
-    
+
   return (
     <div className="App">
       <h2>My tasks</h2>
       <CategoryFilter 
-      categories={CATEGORIES}
-      selectedCategory={selectedCategory}
-      onCategoryClick={handleCategoryClick} 
+        categories={CATEGORIES}
+        selectedCategory={selectedCategory}
+        onCategoryClick={handleCategoryClick} 
       />
       <NewTaskForm 
-      categories={CATEGORIES.filter(cat => cat !== 'All')}
-      onTaskFormSubmit={handleTaskFormSubmit} 
+        categories={CATEGORIES.filter(cat => cat !== 'All')}
+        onTaskFormSubmit={handleTaskFormSubmit} 
       />
       <TaskList tasks={filteredTasks} onDelete={handleDelete} />
     </div>
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -2,6 +2,7 @@ import React from "react";
 import Task from "./Task";
 
 function TaskList({ tasks, onDelete }) {
+
   return (
     <div className="tasks">
       {tasks.map(task => ( 
